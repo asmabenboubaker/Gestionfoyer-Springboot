2b43fc301ad1b4ba787cc139121d4c965f219ed7
Asma Ben Boubaker <36500896+asmabenboubaker@users.noreply.github.com>
2023-12-04 20:37:21 +0100

    excel generator + pdf generator

--- src/main/java/esprit/tn/springdemo/controllers/PdfGenerator.java
+++ src/main/java/esprit/tn/springdemo/controllers/PdfGenerator.java
@@ -2,6 +2,9 @@ package esprit.tn.springdemo.controllers;
 
 
 import com.lowagie.text.DocumentException;
+import esprit.tn.springdemo.entities.Bloc;
+import esprit.tn.springdemo.repositories.BlocRepo;
+import esprit.tn.springdemo.services.ExcelGenerator;
 import esprit.tn.springdemo.services.PDFGeneratorService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
@@ -13,14 +16,18 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 
 @RequestMapping("")
 public class PdfGenerator {
     private final PDFGeneratorService pdfGeneratorService;
-    public PdfGenerator(PDFGeneratorService pdfGeneratorService) {
+    private final BlocRepo blocRepo;
+    public PdfGenerator(PDFGeneratorService pdfGeneratorService,BlocRepo blocRepo) {
         this.pdfGeneratorService = pdfGeneratorService;
+
+        this.blocRepo=blocRepo;
     }
     @GetMapping("/export/pdf")
     public void exportToPDF(HttpServletResponse response) {
@@ -37,6 +44,20 @@ public class PdfGenerator {
         } catch (IOException | DocumentException e) {
             e.printStackTrace(); // Log the exception or handle it appropriately
         }
+    }
+    @GetMapping("/export-to-excel")
+    public void exportIntoExcelFile(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=blocs" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<Bloc> etudiantList = (List<Bloc>) blocRepo.findAll();
+        ExcelGenerator generator = new ExcelGenerator(etudiantList);
+        generator.generateExcelFile(response);
     }
 
 }
